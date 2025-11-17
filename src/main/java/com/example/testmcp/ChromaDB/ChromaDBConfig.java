b95c5f3b1a616dccfec8b58e95e5ea17e3b13a1d
Zahaawii <zahaawii@gmail.com>
2025-11-17 17:04:09 +0100

    Finally managed to add the connection between spring and chromadb - there standard way to do it doenst work, so I do it manually with restclient: I do recommend using the standard solution, when it works. - Next step is implementing a record that takes the infomartion and store them and make it possible for the MCP server to be grabbed by the LLM

---
+++ src/main/java/com/example/testmcp/ChromaDB/ChromaDBConfig.java
@@ -0,0 +1,44 @@
+
+package com.example.testmcp.ChromaDB;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.ai.chroma.vectorstore.ChromaApi;
+import org.springframework.ai.chroma.vectorstore.ChromaVectorStore;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+
+@Configuration
+public class ChromaDBConfig {
+
+    private String chromaApiToken = System.getenv("CHROMADB_API_KEY");
+
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder()
+                .requestFactory(new SimpleClientHttpRequestFactory())
+                .defaultHeader("X-Chroma-Token", chromaApiToken);
+    }
+
+    @Bean
+    public ChromaApi chromaApi(RestClient.Builder restClientBuilder) {
+        String chromaUrl = "https://api.trychroma.com";
+        return new ChromaApi(chromaUrl, restClientBuilder, new ObjectMapper());
+    }
+
+    @Bean
+    public VectorStore chromaVectorStore(EmbeddingModel embeddingModel, ChromaApi chromaApi) {
+        return ChromaVectorStore.builder(chromaApi, embeddingModel)
+                .tenantName("37f4ff04-8a40-4a5c-ba87-77b9e4b5d60d")
+                .databaseName("Test")
+                .collectionName("test_api")
+                .initializeSchema(false)
+                .build();
+    }
+}
--- src/main/java/com/example/testmcp/testController.java
+++ src/main/java/com/example/testmcp/testController.java
@@ -3,13 +3,22 @@ package com.example.testmcp;
 
 import com.example.testmcp.entity.Blog;
 import com.example.testmcp.repositories.BlogRepository;
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class testController {
+
+    @Autowired
+    VectorStore vectorStore;
 
     private final BlogRepository blogRepository;
 
@@ -20,5 +29,18 @@ public class testController {
     @GetMapping("")
     public List<Blog> getAll() {
         return blogRepository.findAll();
+    }
+
+    @GetMapping("/test")
+    public List<Document> test() {
+//        List<Document> documents = List.of(
+//                new Document("Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!!", Map.of("meta1", "meta1")),
+//                new Document("The World is Big and Salvation Lurks Around the Corner"),
+//                new Document("You walk forward facing the past and you turn back toward the future.", Map.of("meta2", "meta2")));
+//
+//        vectorStore.add(documents);
+
+        List<Document> results = vectorStore.similaritySearch("spring");
+        return results;
     }
 }
