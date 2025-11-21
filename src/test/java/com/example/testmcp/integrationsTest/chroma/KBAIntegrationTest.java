2b36a4a15845e6aaff5d0178a9f086ac18e5bf39
Zahaawii <zahaawii@gmail.com>
2025-11-21 22:28:15 +0100

    changing the integrationtest for KBA

--- src/test/java/com/example/testmcp/integrationsTest/chroma/KBAIntegrationTest.java
+++ src/test/java/com/example/testmcp/integrationsTest/chroma/KBAIntegrationTest.java
@@ -3,11 +3,15 @@ package com.example.testmcp.integrationsTest.chroma;
 
 import com.example.testmcp.ChromaDB.ChromaDBConfig;
 import com.example.testmcp.mcpService.KBAServiceMCP;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.chroma.vectorstore.ChromaApi;
 import org.springframework.ai.chroma.vectorstore.ChromaVectorStore;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.filter.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -16,9 +20,9 @@ import org.springframework.web.client.RestClient;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 
 @SpringBootTest
 public class KBAIntegrationTest {
@@ -33,10 +37,11 @@ public class KBAIntegrationTest {
     KBAServiceMCP MCP;
 
     @Autowired
-    private EmbeddingModel embeddingModel;
+    VectorStore vectorStore;
 
     @Autowired
-    private ChromaApi chromaApi;
+    private EmbeddingModel embeddingModel;
+
 
     @Test
     void context() {
@@ -69,30 +74,36 @@ public class KBAIntegrationTest {
     void restClientBuilderWorks() {
         String header = "X-Chroma-Token";
         String apiToken = System.getenv("CHROMADB_API_KEY");
-        String expectedRestClientResult = "web.client";
-        String expectedChromaResult = "web.client";
 
-        String restClientConnection = config.restClientBuilder().toString();
-
+        RestClient.Builder restClientConnection = config.restClientBuilder();
         RestClient.Builder chromaAPIConnection = RestClient.builder().requestFactory(new SimpleClientHttpRequestFactory())
                 .defaultHeader(header, apiToken);
 
         //IF the connection doesn't work, we will receive an error, so it is testable like this
         assertNotNull(restClientConnection);
         assertNotNull(chromaAPIConnection);
-        assertTrue(restClientConnection.contains(expectedRestClientResult));
-        assertTrue(chromaAPIConnection.toString().contains(expectedChromaResult));
     }
 
     @Test
     void connectToChromaDB() {
-        String tenantName = "37f4ff04-8a40-4a5c-ba87-77b9e4b5d60d";
-        String databaseNameTest = "Test";
-        String colletionNameTest = "test_api";
 
-        assertNotNull(ChromaVectorStore.builder(chromaApi, embeddingModel).tenantName(tenantName)
-                .databaseName(databaseNameTest)
-                .collectionName(colletionNameTest).initializeSchema(false).build());
+        //if all these steps go through the connection works
+        String id = "integrationtest_" + UUID.randomUUID();
+        String content = "Test article for integration test";
+        String tagName = "test";
+        String tagDescription = "test";
+        List<Document> documents = List.of(new Document(id, content, Map.of(tagName, tagDescription)));
+
+
+        vectorStore.add(documents);
+
+        List<Document> result = vectorStore.similaritySearch(id);
+        assertFalse(result.isEmpty());
+        assertEquals(id, result.getFirst().getId());
+
+        vectorStore.delete(List.of(id));
+
     }
 
 }
+
